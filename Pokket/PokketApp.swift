6ec7fa40d1f550e634c72012c9314ec192d91bc9
Marvin Huynh <huynhtuhoangminh@gmail.com>
2024-04-19 16:56:23 -0700

    Initial Commit

---
+++ Pokket/PokketApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokketApp.swift
+//  Pokket
+//
+//  Created by Minh Huynh on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct PokketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

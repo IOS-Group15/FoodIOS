36c0d0d53450c67f8b5ca9a7f3fdfc5781273e10
Marvin Huynh <huynhtuhoangminh@gmail.com>
2024-04-26 17:54:33 -0700

    remove duplicated navigation stack causing the app name not showing add missing file reference lead to unable to run the project

--- Pokket/PokketApp.swift
+++ Pokket/PokketApp.swift
@@ -10,25 +10,21 @@ import FirebaseCore
 
 @main
 struct PokketApp: App {
-
+    
     @State private var authViewModel: AuthViewModel
-
+    
     init() {
         FirebaseApp.configure()
         authViewModel = AuthViewModel()
     }
-
+    
     var body: some Scene {
         WindowGroup {
             if authViewModel.user != nil { // <-- Check if you have a non-nil user (means there is a logged in user)
-
-                // We have a logged in user, go to MainView
-                NavigationStack {
-                    MainView()
-                        .environment(authViewModel)
-                }
+                MainView()
+                    .environment(authViewModel)
             } else {
-
+                
                 // No logged in user, go to LoginView
                 LoginView()
                     .environment(authViewModel)

7abba888d3142a5ca539e525f49fc801e82a32da
Marvin Huynh <huynhtuhoangminh@gmail.com>
2024-04-27 00:00:25 -0700

    add firebase integration add add budget view

---
+++ Pokket/AddBudgetView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBudgetView.swift
+//  Pokket
+//
+//  Created by Minh Huynh on 4/26/24.
+//
+
+import SwiftUI
+
+struct AddBudgetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBudgetView()
+}
---
+++ Pokket/Elements.swift
@@ -0,0 +1,18 @@
+//
+//  Elements.swift
+//  Pokket
+//
+//  Created by Minh Huynh on 4/26/24.
+//
+
+import SwiftUI
+
+struct Elements: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Elements()
+}
